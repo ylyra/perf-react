cd00138fc9415342d3dc85d5f190b551d3b18e73
Yan Lyra <yanlyra3@gmail.com>
2021-11-28 20:23:58 -0300

    ⚗️ Testing react-virtualized on SearchResults component

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { AutoSizer, List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultsProps = {
@@ -17,17 +17,36 @@ export function SearchResults({
   results,
   onAddToWishlist,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    );
+  };
+
   return (
     <section>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => (
+      {/*{results.map((product) => (
         <ProductItem
           key={product.id}
           product={product}
           onAddToWishlist={onAddToWishlist}
         />
-      ))}
+      ))}*/}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </section>
   );
 }
