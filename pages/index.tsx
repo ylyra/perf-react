7bf123730fb3ea58d4e4e087d90db669aef91c9c
Yan Lyra <yanlyra3@gmail.com>
2021-11-28 17:49:15 -0300

    ➕ Added axios to handle api requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
+import { api } from "../services/api";
+
+type ResultsData = {
+  id: number;
+  price: number;
+  title: string;
+};
 
 const Home: NextPage = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState<ResultsData[]>([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) return;
+
+    try {
+      const response = await api.get<ResultsData[]>("products", {
+        params: {
+          q: search.trim(),
+        },
+      });
+
+      setResults(response.data);
+    } catch {}
+  }
+
   return (
     <div>
       <Head>
@@ -11,7 +39,19 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Hello World</h1>
+        <h1>Search</h1>
+
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <button>Buscar</button>
+        </form>
+
+        <SearchResults results={results} />
       </main>
     </div>
   );
