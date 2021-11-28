9d1f37d0fda4bcf688d74489eee949fddbb8e14f
Yan Lyra <yanlyra3@gmail.com>
2021-11-28 20:10:03 -0300

    ⚗️ Testing code splitting

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,4 +1,17 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import dynamic from "next/dynamic";
+
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  async () => {
+    const mod = await import("./AddProductToWishlist");
+    return mod.AddProductToWishlist;
+  },
+  {
+    loading: () => <span>carregando</span>,
+  }
+);
 
 type ProductItemProps = {
   product: {
@@ -11,12 +24,20 @@ type ProductItemProps = {
 };
 
 function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>{" "}
-      <button onClick={() => onAddToWishlist(product.id)}>
-        Add to wishlist
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
       </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
     </div>
   );
 }
